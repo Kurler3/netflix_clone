f0e7008e91b44c2025c7d70b76cf5fa1744ea3c8
Kurler3 <miguelbento2000@gmail.com>
2022-06-10 15:28:28 +0100

    Finished header design

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,11 +1,13 @@
 import React, {memo} from 'react';
+import {BellIcon, SearchIcon} from '@heroicons/react/solid';
+import Link from 'next/link';
 
 
 const Header = () => {
   return (
     <header> 
         {/* LEFT SIDE */}
-        <div>
+        <div className='flex items-center space-x-2 md:space-x-10'>
             <img 
                 src="https://rb.gy/ulxxee"
                 width={100}
@@ -14,19 +16,33 @@ const Header = () => {
             />
             
             {/* LINKS */}
-            <ul>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
+            <ul className='hidden md:flex space-x-4'>
+                <li className='headerLink'>Home</li>
+                <li className='headerLink'>TV Shows</li>
+                <li className='headerLink'>Movies</li>
+                <li className='headerLink'>New & Popular</li>
+                <li className='headerLink'>My List</li>
             </ul>
         </div>
 
         {/* RIGHT SIDE */}
-        <div>
-
+        <div className='flex items-center space-x-4 text-sm font-light'>
+          {/* SEARCH ICON */}
+          <SearchIcon className="hidden sm:inline h-6 w-6 "/>
+          {/* KIDS */}
+          <p className='hidden lg:inline'>Kids</p>
+          {/* NOTIFICATIONS ICON */}
+          <BellIcon className='h-6 w-6'/>
+          {/* LINK TO ACCOUNT */}
+          <Link href='/account'>
+            <img 
+              src="https://rb.gy/g1pwyx"
+              alt=""
+              className="cursor-pointer rounded"
+            />
+          </Link>
         </div>
+
     </header>
   )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,7 @@ import Header from '../components/Header'
 
 const Home: NextPage = () => {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+    <div className='p-2'>
       <Head>
         <title>Netflix Clone</title>
         <link rel="icon" href="/favicon.ico" />
